9c7a7f353c3d844ca973d783a5ed03bd4c8d779a
Alex Pop <palexandru@gmail.com>
2016-11-02 21:59:30 +0000

    Chapter 4, Example 1 - initial commit

---
+++ Chapter4/Example1/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <div class="container">
+      <div class="page-header">
+        <h1>Mastering Angular 2 - Chapter 4, Example 1</h1>
+      </div>
+      <p class="lead">This is a minimal example for an Angular 2 component using styling provided by the popular Bootstrap framework.</p>
+    </div>    
+    `
+})
+export class AppComponent { }
