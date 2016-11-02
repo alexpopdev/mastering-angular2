5b4bd65fdb20e8d4b9e3f191a656188d2794ece9
Alex Pop <palexandru@gmail.com>
2016-11-02 22:29:53 +0000

    Chapter 4, Example 1 - minor wording changes

--- Chapter4/Example1/app/app.component.ts
+++ Chapter4/Example1/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       <div class="page-header">
         <h1>Mastering Angular 2 - Chapter 4, Example 1</h1>
       </div>
-      <p class="lead">This is a minimal example for an Angular 2 component using styling provided by the popular Bootstrap framework.</p>
+      <p class="lead">This is a minimal example for an Angular 2 componentwith an element tag selector.</p>
     </div>    
     `
 })
