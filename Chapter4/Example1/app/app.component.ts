846a79058d3df375533a60c0e4eab03fc5097800
Alex Pop <palexandru@gmail.com>
2016-11-03 21:33:08 +0000

    Chapter 4, Example 1 - added databinding expressions

--- Chapter4/Example1/app/app.component.ts
+++ Chapter4/Example1/app/app.component.ts
@@ -3,12 +3,32 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div class="container">
-      <div class="page-header">
-        <h1>Mastering Angular 2 - Chapter 4, Example 1</h1>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="page-header">
+            <h1>{{title}}</h1>
+          </div>
+          <p class="lead">{{description}}</p>
+        </div>
       </div>
-      <p class="lead">This is a minimal example for an Angular 2 componentwith an element tag selector.</p>
+      <div class="row">
+        <div class="col-md-6">
+          <p>A child component could go here</p>
+        </div>
+        <div class="col-md-6">
+          <p>Another child component could go here</p>
+        </div>
+      </div>          
     </div>    
     `
 })
-export class AppComponent { }
+export class AppComponent { 
+  title: string;
+  description: string;
+
+  constructor(){
+    this.title = 'Mastering Angular 2 - Chapter 4, Example 1';
+    this.description = 'This is a minimal example for an Angular 2 component with an element tag selector.';
+  }
+}
