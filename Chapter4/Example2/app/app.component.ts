0e7598fb9c250c017169bc3e7d1c2adc0eb92167
Alex Pop <palexandru@gmail.com>
2016-11-06 16:26:28 +0000

    Chapter 4, Example 2: changed to a more expressive event handler method name

--- Chapter4/Example2/app/app.component.ts
+++ Chapter4/Example2/app/app.component.ts
@@ -4,38 +4,31 @@ import { Component } from '@angular/core';
     selector: 'div.container.my-app',
     template: `
     <div class="container text-center">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="page-header">
-            <h1>{{title}}</h1>
-          </div>
+      <div class="row"><div class="col-md-12">
+          <div class="page-header"><h1>{{title}}</h1></div>
           <p class="lead">{{description}}</p>
-         </div>
-      </div>
+      </div></div>
       <div class="row">
-        <div class="col-md-6" my-child-comp myText="A child component goes here" (onChildMessage)="onChildMessage($event)"></div>      
-        <div class="col-md-6" my-child-comp myText="Another child component goes here" (onChildMessage)="onChildMessage($event)"></div>         
+        <div class="col-md-6" my-child-comp myText="A child component goes here" (onChildMessage)="onChildMessageReceived($event)"></div>      
+        <div class="col-md-6" my-child-comp [myText]="secondComponentText" (onChildMessage)="onChildMessageReceived($event)"></div>         
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="well well-sm">         
+      <div class="row"><div class="col-md-12"><div class="well well-sm">         
             <p>Last message from child components: <strong>{{lastMessage}}</strong></p>
-          </div>
-        </div>
-      </div>          
-    </div>    
-    `
+      </div></div></div>          
+    </div>`
 })
 export class AppComponent { 
   title: string;
   description: string;
+  secondComponentText: string;
   lastMessage: string;
   constructor(){
     this.title = 'Mastering Angular 2 - Chapter 4, Example 2';
     this.description = 'This is an example for an Angular 2 root component with an element and class selector and a child component with an element attribute selector.';
+    this.secondComponentText = 'Another child component goes here';
   }
 
-  onChildMessage($event: string)
+  onChildMessageReceived($event: string)
   {
     this.lastMessage = $event;
   }
