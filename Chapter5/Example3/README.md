c9e03d1fb827a255e8720503d103001e60471324
Alex Pop <palexandru@gmail.com>
2017-01-22 12:22:46 +0000

    Chapter 5, Example3: baseline

---
+++ Chapter5/Example3/README.md
@@ -0,0 +1,7 @@
+# Mastering Angular 2 - Chapter 5, Example 3
+
+This is an example for built in structural directives. 
+
+Prerequisites: Node.js and TypeScript (see Chapter 1 for step by step environment setup)
+
+Execute `npm install` and `npm run start` to install the required dependencies and see the example in action.
---
+++ Chapter5/Example3/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'div.container.text-center.my-app',
+  template: `    
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-header">
+          <h1>{{title}}</h1></div>
+          <p class="lead">{{description}}</p>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-md-12">
+        <p>
+          <label for="message-input">Customize message:</label>
+          <input id="message-input" type="text" [(ngModel)]="message">
+         </p>
+         <p>
+          <button [disabled]="!message" class="btn btn-default" type="button" (click)="onMessageSent(message)">Send message</button>
+        </p>        
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="well well-sm"><p>Last message received: <strong>{{lastMessage}}</strong></p></div>
+      </div>
+    </div>`
+})
+export class AppComponent{
+  title: string;
+  description: string;
+  message: string;
+  lastMessage: string;
+
+  constructor() {
+    this.title = 'Mastering Angular 2 - Chapter 5, Example 3';
+    this.description = 'This is an example for built in structural directives.';
+  }
+    
+  onMessageSent($event: string) {
+    this.lastMessage = $event;
+  }
+}
